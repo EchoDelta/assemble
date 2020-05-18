35e4e7702b0ffab5f4e96813074d23ac53520f19
Eirik J. Daling <eirik@daling.net>
2020-05-18 09:30:53 +0200

    Renaming variable

--- src/Assemble.Desktop/Extensions/IsometricExtensions.cs
+++ src/Assemble.Desktop/Extensions/IsometricExtensions.cs
@@ -8,16 +8,16 @@ namespace Assemble.Desktop.Extensions
         private const float IsometricTileWidth = 45.2550f;
         private const float IsometricTileHeight = 22.6275f;
 
-        public static Transform2 ToIsometric(this Transform2 worldPos)
+        public static Transform2 ToIsometric(this Transform2 tilePos)
         {
-            return new Transform2(worldPos.Position.ToIsometric());
+            return new Transform2(tilePos.Position.ToIsometric());
         }
 
-        public static Vector2 ToIsometric(this Vector2 worldPos)
+        public static Vector2 ToIsometric(this Vector2 tilePos)
         {
             return new Vector2(
-                (worldPos.X - worldPos.Y) * IsometricTileWidth,
-                (worldPos.X + worldPos.Y) * IsometricTileHeight);
+                (tilePos.X - tilePos.Y) * IsometricTileWidth,
+                (tilePos.X + tilePos.Y) * IsometricTileHeight);
         }
     }
 }
