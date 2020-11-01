d956cf8fc9261bcb37933317ef7c81b94f564017
Eirik Jenssen Daling <eirik.daling@bekk.no>
2020-11-01 13:27:11 +0100

    Trying a new way of calculating depth

--- src/Assemble.Desktop/DepthHelper.cs
+++ src/Assemble.Desktop/DepthHelper.cs
@@ -1,18 +1,40 @@
+using System;
 using Assemble.Desktop.Components;
+using Assemble.Desktop.Enums;
 
 namespace Assemble.Desktop
 {
     public class DepthHelper
     {
         private float _heightModifier;
+        private float _layerModifier;
         public DepthHelper((int x, int y) mapSize)
         {
             _heightModifier = 1.0f / (mapSize.x + mapSize.y);
+            _layerModifier = _heightModifier / (Enum.GetNames(typeof(TileRenderLayerType)).Length + 1);
         }
 
-        public float GetDepth(TilePosition tilePosition)
+        public float GetDepth(TilePosition tilePosition, TileRenderLayer layer)
+        {
+            return GetPositionDepth(tilePosition) + GetLayerDepth(layer);
+        }
+
+        private float GetPositionDepth(TilePosition tilePosition)
         {
             return (tilePosition.Position.X + tilePosition.TileSpan.X + tilePosition.Position.Y + tilePosition.TileSpan.Y) * _heightModifier;
+        }
+
+        private float GetLayerDepth(TileRenderLayer layer)
+        {
+
+            return layer?.Layer switch
+            {
+                TileRenderLayerType.GroundTile => 0 * _layerModifier,
+                TileRenderLayerType.Resources => 1 * _layerModifier,
+                TileRenderLayerType.Units => 2 * _layerModifier,
+                TileRenderLayerType.Overlay => 3 * _layerModifier,
+                _ => 4 * _layerModifier,
+            };
         }
     }
 }
